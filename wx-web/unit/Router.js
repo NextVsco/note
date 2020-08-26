4055b7bc47fec4addf3ff3c9c1c1d2fd9ca74c02
一颗猫粮 <1157460105@qq.com>
2020-08-26 18:26:38 +0800

    ..

--- wx-web/unit/Router.js
+++ wx-web/unit/Router.js
@@ -1,4 +1,4 @@
-import Filter from "./route-filter";
+import Filter from "./Router-filter";
 
 const point = wx;
 const keys = ["url", "appid", "mold", "success", "fail", "complete"]
@@ -7,12 +7,19 @@ const modeType = {
   back: { keys, carry: function (param) { wx.navigateBack(param); } },
   redirect: { keys, necessity: "url", carry: function (param) { wx.redirectTo(param); } },
   reLaunch: { keys, carry: function (param) { wx.reLaunch(param); } },
-  toTabbar: { keys, necessity: "url", carry: function (param) { wx.switchTab(param); } }
+  tabbar: { keys, necessity: "url", carry: function (param) { wx.switchTab(param); } },
+  app: { keys, necessity: "appid", carry: function (param) { } },
+  phone: { keys, necessity: "url", carry: function (param) { } },
+  picture: { keys, necessity: "url", carry: function (param) { } },
+  location: { keys, necessity: "url", carry: function (param) { } }
 }
 /**
  * Route 小程序路由 注册router
+ * 基本介绍：
+ * 防抖，防止短暂时间内弹出多个窗口
+ * <getData> 过滤获取数据，小程序view返回到事件中的数据包含其他多余信息
  */
-class Route extends Filter {
+class Router extends Filter {
   constructor(e = {}) {
     point.router = this
   }
@@ -23,23 +30,32 @@ class Route extends Filter {
    */
   toPage(e) {
     var data = getData(e)
-
+    data = defaultFill(data)
+    // 执行
+  }
+}
+/**
+ * defaultFill 数据类型完善
+ * @param {*} data 
+ */
+function defaultFill(data){
+  if(data.mold==undefined){
 
   }
 }
-
 /**
  * 对多样的数据进行统一处理
  * @param {*} param
  */
 function getData(param) {
-  return param
+  return param.currentTarget ? param.currentTarget.dataset : param
 }
 
+// url地址string转json
 function urlParse(param) {
 
 }
-
-function urlMerge(param) {
+// url地址json转string
+function urlMerge(url,param) {
 
 }
