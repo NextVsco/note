ea9566c13becad11c2682d21444117b5d4f3b0c8
NextVsco <1157460105@qq.com>
2020-12-21 21:24:12 +0800

    ...

--- prime/statistics.js
+++ prime/statistics.js
@@ -1,18 +1,27 @@
 const { list } = require("./resource");
 
 var num = 0;
-var target = "100100101111000100010011100001"
+var target = "11111111111111111111111111111111111111111"
+// var target2 = "164511353"
 
-console.log(target, target.length)
-for (var i = target.length - 1; i >= 0; i--) {
-  num += ((target[i] == "1") ? Math.pow(2, target.length - 1 - i) : 0)
-}
-console.log(num)
-
-for (var i in list) {
-  let value = parseInt(list[i], 2)
-  if (num % value == 0) {
-    console.log('ero', value, num / value)
-    break
+function searchChildren(target){
+  console.log("target =>",target, target.length)
+  for (var i = target.length - 1; i >= 0; i--) {
+    num += ((target[i] == "1") ? Math.pow(2, target.length - 1 - i) : 0)
   }
+  console.log(num)
+  
+  for (var i in list) {
+    let value = parseInt(list[i], 2)
+    if (num % value == 0) {
+      console.log('ero', value, num / value)
+      break
+    }
+  }
+}
+
+if (!(typeof target == "undefined")) {
+  searchChildren(target)
+} else if (!(typeof target2 == "undefined")) {
+  searchChildren(parseInt(target2).toString(2))
 }
