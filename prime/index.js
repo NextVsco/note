3370e05766c9d22138e2f32ae10bcb042cc28448
一颗猫粮 <1157460105@qq.com>
2020-12-24 17:14:47 +0800

    ...

--- prime/index.js
+++ prime/index.js
@@ -1,4 +1,51 @@
 // const DigPrime = require("./dig");
 // new DigPrime().main()
 
-require("./statistics");
+// require("./statistics");
+
+// let i = 0
+// do {
+//   i++
+//   console.log(
+//     // i,
+//     i.toString(2)
+//   )
+//   let powNum = Math.pow(i, 2)
+//   console.log(
+//     // powNum,
+//     powNum.toString(2)
+//   )
+//   console.log(
+//     // powNum,
+//     Math.pow(i, 3).toString(2)
+//   )
+//   console.log(
+//     // powNum,
+//     Math.pow(i, 4).toString(2)
+//   )
+//   console.log()
+// } while (i < 32)
+
+
+function formatTime(date, fmt = "YYYY-mm-dd") {
+  if (typeof date == "number") { date = new Date(date) }  // date自动转换
+  var ret;
+  var opt = {
+    "Y+": date.getFullYear().toString(),        // 年
+    "m+": (date.getMonth() + 1).toString(),     // 月
+    "d+": date.getDate().toString(),            // 日
+    "H+": date.getHours().toString(),           // 时
+    "M+": date.getMinutes().toString(),         // 分
+    "S+": date.getSeconds().toString()          // 秒
+    // 有其他格式化字符需求可以继续添加，必须转化成字符串
+  };
+  for (var k in opt) {
+    ret = new RegExp("(" + k + ")").exec(fmt);
+    if (ret) {
+      fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+    };
+  };
+  return fmt;
+}
+
+console.log(formatTime(1579708800000,"d天"))
--- prime/statistics.js
+++ prime/statistics.js
@@ -1,18 +1,32 @@
 const { list } = require("./resource");
 
-var num = 0;
-var target = "100100101111000100010011100001"
-
-console.log(target, target.length)
-for (var i = target.length - 1; i >= 0; i--) {
-  num += ((target[i] == "1") ? Math.pow(2, target.length - 1 - i) : 0)
-}
-console.log(num)
-
-for (var i in list) {
-  let value = parseInt(list[i], 2)
-  if (num % value == 0) {
-    console.log('ero', value, num / value)
-    break
+/* 计算二进制数的因数 */
+function methodOne(target) {
+  target = target || "100100101111000100010011100001"
+  var num = 0;
+  console.log(target, target.length)
+  for (var i = target.length - 1; i >= 0; i--) {
+    num += ((target[i] == "1") ? Math.pow(2, target.length - 1 - i) : 0)
+  }
+  console.log(num)
+  
+  for (var i in list) {
+    let value = parseInt(list[i], 2)
+    if (num % value == 0) {
+      console.log('ero', value, num / value)
+      break
+    }
   }
 }
+
+/* 遍历一定范围内的最大因数 */
+function methodTwo(max, min = 1) {
+  var i = min
+  do {
+    i++
+    console.log(i,i.toString(2))
+  } while (i < max)
+
+}
+
+module.exports = { methodOne, methodTwo }
