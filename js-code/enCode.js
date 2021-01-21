3314295c343ce19636d8ebc5423235257cbee770
一颗猫粮 <1157460105@qq.com>
2021-01-21 17:39:35 +0800

    ...

--- js-code/enCode.js
+++ js-code/enCode.js
@@ -1,106 +1,19 @@
-/* 查找obj对象 */
-function selectObject(obj, keyArray) {
-  function jsonStringKey(key) {
-    let stop = false
-    do {
-      let value = key.match(/\[[\d]+\]/)
-      if (value) {
-        key = key.substring(0, value.index) + "." + value[0].substring(1, value[0].length - 1) + key.substring(value.index + value[0].length)
-      } else {
-        stop = true
-      }
-    } while (!stop);
-    return key.split(".")
-  }
-  if(typeof keyArray == "string") { keyArray = jsonStringKey(keyArray) }
-  for (var i = 0; i < keyArray.length; i++) {
-    obj = obj[keyArray[i]]
-  }
-  return obj
-}
+var { codeType, selectObj } = require("./enCodeUnit");
 
-/* 字符串char类型 */
-function codeType(char) {
-  if (typeof char == "string") { char = char.charCodeAt() }
-  if (char == 32) {
-    return "space" // 空格
-  } else if (char == 34) {
-    return "dquote" // 双引号
-  } else if (char == 39) {
-    return "squote" // 单引号
-  } else if (char == 40 || char == 41) {
-    return "sbracket" // 小括号
-  } else if (char == 44) {
-    return "comma" // 逗号
-  } else if (char == 46) {
-    return "spot" // 点
-  } else if (char >= 48 && char <= 57) {
-    return "number" // 数字
-  } else if (char == 58) {
-    return "colon" // 冒号
-  } else if (char == 59) {
-    return "semicolon" // 分号
-  } else if (char >= 65 && char <= 122) {
-    return "char" // 字符
-  } else if (char == 91 || char == 93) {
-    return "mbracket" // 中括号
-  } else if (char == 96) {
-    return "rquote" // 反引号
-  } else if (char == 123 || char == 125) {
-    return "lbracket" // 大括号
-  }
-}
-
-/**
- * {name:"a",sex:1}
- * app.toPage(wx.getHomeToPage())
- * app.toPage({url:wx.once(),mold:wx.two(wx.moldType(wx.three())),success:wx.back})
- */
 /* run */
 var text = `app.toPage({url:app.globalData.HOME,mold:console.log("hahaha")});console.log("aaaa")`
 text = `app[0]["name"].toPage({url:wx.once(),mold:wx.two(wx.moldType(wx.three())),success:wx.back})`
 text = `app.system.getPayNum(math.random())`
-text = `math.random()`
+text = `("app.id")`
+// text = `app.getObj({id:app.id,v:app.v})`
 
-function builtObj(str) {
-  switch (str) {
-    case "app":
-      return {
-        id: "452sas4d2",
-        v: "1.0.0",
-        system: {
-          language: "zh_CN",
-          model: "iPhone 6/7/8 Plus",
-          pixelRatio: 3,
-          platform: "devtools",
-          getPayNum: function (num="335") {
-            return parseInt(num.toString().substring(2,7)).toString(2)
-          }
-        }
-      }
-    case "math":
-      return Math;
-    default:
-      break;
-  }
-}
-
-function selectObj(strs) {
-  console.log(strs)
-  var m = strs.match(/[\w]+[.|[]/)
-  if (m) {
-    var name = m[0].substring(0, m[0].length - 1)
-    console.log("m=>",name,strs)
-    return selectObject({ [name]: builtObj(name) },strs)
-  }
-  return builtObj(strs)
-}
-
+console.log(
+  eatCodeString(text).value
+)
 
 /* 不支持换行和空格（字符串内除外），支持已定义对象内变量、函数的调用，JSON */
 function eatCodeString(text) {
   var str = ""
-  var wait = ""
   var obj = {}
   var index = 0
   var point = null
@@ -112,29 +25,33 @@ function eatCodeString(text) {
     if (
       charType == "char" ||
       charType == "number" ||
-      // 特殊符
       charType == "spot" ||
       charType == "mbracket" ||
       charType == "space"
     ) {
       str += char
     } else {
-      if (charType == "lbracket") {
-        if (char == "{") {
+      if (charType == "sbracket") {
+        if (char == "(") {
+          point = selectObj(str)
           /* recursion */
           var recursion = eatCodeString(text)
           index += recursion.index
           text = text.substring(recursion.index)
           value = recursion.value
-        } else if (char == "}") {
+          console.log("recursion", recursion, value)
           return {
             index,
-            value: obj
+            value: selectObj(str)(value)
+          }
+        } else if (char == ")") {
+          return {
+            index,
+            value: selectObj(str)
           }
         }
-      } else if (charType == "sbracket") {
-        if(char == "(") {
-          point = selectObj(str)
+      } else if (charType == "lbracket") {
+        if (char == "{") {
           /* recursion */
           var recursion = eatCodeString(text)
           index += recursion.index
@@ -144,29 +61,35 @@ function eatCodeString(text) {
             index,
             value: value
           }
-        } else if (char == ")") {
-          console.log(index,value,str)
+        } else if (char == "}") {
           return {
             index,
             value: selectObj(str)
           }
         }
       } else if (charType == "dquote") {
-        console.log("wait plan")
         if (value == null) { value = "" }
-        else { value = str }
+        else {
+          return {
+            index, value: str
+          }
+        }
       } else if (char == ":") {
-        obj[str] = ""
-        wait = str
+        var recursion = eatCodeString(text)
+        index += recursion.index
+        text = text.substring(recursion.index)
+        obj[str] = recursion.value
       } else if (char == ",") {
-        obj[wait] = ""
+        return {
+          index,
+          value: selectObj(str)
+        }
       } else if (char == ";") {
         return {
           index,
           value: point(value)
         }
       } else {
-        console.log("undefined plan", char)
         break
       }
       str = ""
@@ -177,10 +100,3 @@ function eatCodeString(text) {
     value: selectObj(str)
   }
 }
-
-
-console.log(
-  eatCodeString(text)
-  // builtObj("math")
-
-)
---
+++ js-code/enCodeUnit.js
@@ -0,0 +1,97 @@
+/* 查找obj对象 */
+function selectObject(obj, keyArray) {
+  function jsonStringKey(key) {
+    let stop = false
+    do {
+      let value = key.match(/\[[\d]+\]/)
+      if (value) {
+        key = key.substring(0, value.index) + "." + value[0].substring(1, value[0].length - 1) + key.substring(value.index + value[0].length)
+      } else {
+        stop = true
+      }
+    } while (!stop);
+    return key.split(".")
+  }
+  if (typeof keyArray == "string") { keyArray = jsonStringKey(keyArray) }
+  for (var i = 0; i < keyArray.length; i++) {
+    obj = obj[keyArray[i]]
+  }
+  return obj
+}
+
+/* 字符串char类型 */
+function codeType(char) {
+  if (typeof char == "string") { char = char.charCodeAt() }
+  if (char == 32) {
+    return "space" // 空格
+  } else if (char == 34) {
+    return "dquote" // 双引号
+  } else if (char == 39) {
+    return "squote" // 单引号
+  } else if (char == 40 || char == 41) {
+    return "sbracket" // 小括号
+  } else if (char == 44) {
+    return "comma" // 逗号
+  } else if (char == 46) {
+    return "spot" // 点
+  } else if (char >= 48 && char <= 57) {
+    return "number" // 数字
+  } else if (char == 58) {
+    return "colon" // 冒号
+  } else if (char == 59) {
+    return "semicolon" // 分号
+  } else if (char >= 65 && char <= 122) {
+    return "char" // 字符
+  } else if (char == 91 || char == 93) {
+    return "mbracket" // 中括号
+  } else if (char == 96) {
+    return "rquote" // 反引号
+  } else if (char == 123 || char == 125) {
+    return "lbracket" // 大括号
+  }
+}
+
+function builtObj(str) {
+  switch (str) {
+    case "app":
+      return {
+        id: "ID452sas4d2",
+        v: "1.0.0",
+        get: function (t = "null") { return t },
+        getObj: function (t) { return "_" + JSON.stringify(t) },
+        system: {
+          language: "zh_CN",
+          model: "iPhone 6/7/8 Plus",
+          pixelRatio: 3,
+          platform: "devtools",
+          getPayNum: function (num = "335") {
+            return parseInt(num.toString().substring(2, 7)).toString(2)
+          }
+        }
+      }
+    case "math":
+      return Math;
+    default:
+      break;
+  }
+}
+
+/**
+ * 为空下返回空，
+ * 纯数字下返回数字
+ */
+function selectObj(strs) {
+  console.log(strs,!strs)
+  if (!strs) { return (function(e){ return e }) }
+  strs = strs.toString()
+  if (strs.match(/\D/) == null) { return strs }
+  var m = strs.match(/[\w]+[.|[]/)
+  if (m) {
+    var name = m[0].substring(0, m[0].length - 1)
+    var value = selectObject({ [name]: builtObj(name) }, strs)
+    return value
+  }
+  return builtObj(strs)
+}
+
+module.exports = { selectObject, codeType, builtObj, selectObj }
