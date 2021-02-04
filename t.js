1ff1dd8ee5fbddb94ceb5089d4e125f330c051b6
一颗猫粮 <1157460105@qq.com>
2021-02-04 17:56:26 +0800

    ...

--- t.js
+++ t.js
@@ -1,23 +1,36 @@
-var list = []
-
-var data = [
-  {name:1},
-  {name:2},
-  {name:3},
-  {name:2}
-]
-
-function vote(item,item2){
-  return item.name == item2.name
-}
-for(var i=0;i<data.length;i++){
-  let pass = true
-  for(var j=0;j<list.length;j++){
-    if (vote(data[i], list[j])){
-      pass = false
-      break;
-    }
+function timeDown(endDateStr, nowDate = new Date()) {
+  var endDate = new Date(endDateStr);
+  var totalSeconds = parseInt((endDate - nowDate) / 1000);
+  var data = {
+    "day": Math.floor(totalSeconds / (60 * 60 * 24)),                 // 日
+    "hour": Math.floor(totalSeconds % (60 * 60 * 24) / (60 * 60)),     // 时
+    "minute": Math.floor(totalSeconds % (60 * 60 * 24) % (60 * 60) / 60),// 分
+    "second": totalSeconds % (60 * 60 * 24) % (60 * 60) % 60          // 秒
   }
-  pass && list.push(data[i])
+  return data;
 }
-console.log(list)
+
+function getWeekEnd(time = new Date()) {
+  let year = time.getFullYear()
+  let month = time.getMonth() + 1
+  let date = time.getDate()
+  let day = time.getDay()
+  let differ = 6 - day
+  if (differ == 0) { differ = 7 }
+  let weekEnd = (new Date(`${year}-${month}-${date}`).valueOf()) + (differ * 86400000)
+  return weekEnd
+}
+
+for(var i =0;i<24;i++){
+  let value = "2020-2-"+(i+1)
+  var end = getWeekEnd(
+    // new Date("2020-1-30")
+    new Date(value)
+  )
+  console.log(
+    value,
+    new Date(value),
+    new Date(end),
+    timeDown(end,new Date(value))
+  )
+}
